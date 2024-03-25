b78a1ac1f23f9e7326b7fee611a2af2755adf253
Run <runzhu@dreampix.cn>
2024-03-25 15:29:43 +0800

    Initial Commit

---
+++ TanTan/TanTanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanTanApp.swift
+//  TanTan
+//
+//  Created by Run on 2024/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TanTanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
